41b5f4eaeccf42a3e1392047d542d364571b258d
sohaghamjah <aymansohag.contact@gmail.com>
2024-11-04 17:55:59 +0600

    form select option

--- PracticeMVC/Models/Employee.cs
+++ PracticeMVC/Models/Employee.cs
@@ -4,8 +4,13 @@
     {
         public string Name { get; set; }
         public int Age { get; set; }
+        public Gender Gender { get; set; }
         public string Desigation { get; set; }
         public int Salary { get; set; }
 
+    }
+
+    public enum Gender { 
+        Male, Female,
     }
 }
