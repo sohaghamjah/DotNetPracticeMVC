f0de9a775b3914065f82584bac62cf2abb3f2041
sohaghamjah <aymansohag.contact@gmail.com>
2024-11-04 17:45:19 +0600

    working on tag helper form design

---
+++ PracticeMVC/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace PracticeMVC.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Desigation { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
